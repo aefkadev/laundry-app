680d53533a660c9d0b88c84bb36cff7177636944
Fitra Ilyasa <fitrailyasa12@gmail.com>
2023-04-12 21:51:21 +0700

    update fe cms

--- resources/views/admin/pembukuan/create-keluar.blade.php
+++ resources/views/admin/pembukuan/create-keluar.blade.php
@@ -1,6 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.admin.app')
+
+@section('title', 'Pengeluaran')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">

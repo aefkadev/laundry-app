2a3264d809c1fac0c298658c00a4f4b8e4de6ec7
masQ-21dev <mqomarudin0480@gmail.com>
2023-04-12 16:20:18 +0700

    fe commit

---
+++ resources/frontEnd/js/layanan.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.overlay').addEventListener('click', () => {
+        dismissModal()
+    })
+
+    document.getElementById('close-modal').addEventListener('click', () => {
+        dismissModal()
+    })
+
+    document.getElementById('desc-toggle').addEventListener('click', () => {
+        presentModal('modal1')
+    })
+
+    document.getElementById('button-modal-quote-2').addEventListener('click', () => {
+        presentModal('modal-quote-2')
+    })
+})
+
+function presentModal(id) {
+    const modal = document.getElementById(id)
+    const overlay = document.querySelector('.overlay')
+
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+
+    modal.scrollTop = 0
+}
+
+function dismissModal() {
+    document.querySelectorAll('.modal').forEach((modal) => {
+        modal.classList.add('hidden')
+    })
+    document.querySelector('.overlay').classList.add('hidden')
+}

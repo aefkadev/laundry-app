fc778e9f48ba2f7fbb56189882c17da7712e8098
Fitra Ilyasa <fitrailyasa12@gmail.com>
2023-04-25 00:51:41 +0700

    Update menu.blade.php

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -14,7 +14,7 @@
                 <i class="fa-solid fa-bar-chart" aria-hidden="true"></i>
                 <span>Laporan</span>
             </a>
-            <a href="/super/transaksi" class="d-flex flex-column align-items-center text-decoration-none fw-bold" style="color: #f1f1f1;">
+            <a href="/super/transaksi/create" class="d-flex flex-column align-items-center text-decoration-none fw-bold" style="color: #f1f1f1;">
                 <i class="fa-solid fa-receipt"></i>
                 <span>Transaksi</span>
             </a>
@@ -25,19 +25,19 @@
     <section class="pt-3 pb-2 px-3 bg-dark bottom-0 fixed-bottom">
         <div class="navbar p-0 d-lg-block d-xl-none" id="mobile-footer-bar">
             <ul class="container-fluid footer-bar">
-                <a href="/super" class="d-flex flex-column align-items-center text-decoration-none fw-bold" style="color: #f1f1f1;">
+                <a href="/admin" class="d-flex flex-column align-items-center text-decoration-none fw-bold" style="color: #f1f1f1;">
                     <i class="fa-solid fa-house"></i>
                     <span>Beranda</span>
                 </a>
-                <a href="/super/chart" class="d-flex flex-column align-items-center text-decoration-none fw-bold" style="color: #f1f1f1;">
+                <a href="/admin/chart" class="d-flex flex-column align-items-center text-decoration-none fw-bold" style="color: #f1f1f1;">
                     <i class="fa-solid fa-line-chart"></i>
                     <span>Chart</span>
                 </a>
-                <a href="/super/laporan" class="d-flex flex-column align-items-center text-decoration-none fw-bold" style="color: #f1f1f1;">
+                <a href="/admin/laporan" class="d-flex flex-column align-items-center text-decoration-none fw-bold" style="color: #f1f1f1;">
                     <i class="fa-solid fa-bar-chart" aria-hidden="true"></i>
                     <span>Laporan</span>
                 </a>
-                <a href="/super/input-transaksi" class="d-flex flex-column align-items-center text-decoration-none fw-bold" style="color: #f1f1f1;">
+                <a href="/admin/transaksi/create" class="d-flex flex-column align-items-center text-decoration-none fw-bold" style="color: #f1f1f1;">
                     <i class="fa-solid fa-receipt"></i>
                     <span>Input Transaksi</span>
                 </a>

d5f055d5e1e878978112f99994e151d58e03a957
Fitra Ilyasa <fitrailyasa12@gmail.com>
2023-04-28 21:40:50 +0700

    Update AdminSubLayananController.php

--- app/Http/Controllers/Admin/AdminSubLayananController.php
+++ app/Http/Controllers/Admin/AdminSubLayananController.php
@@ -56,51 +56,47 @@ class AdminSubLayananController extends Controller
     }
 
     public function update(Request $request, string $id)
-{
-    $sublayanan = SubLayanan::where('id', $id)->first();
-    $sublayanan->update(
-        [
-            'layanan_id' => $request->layanan_id,
-            'nama_sub' => $request->nama_sub,
-            'deskripsi_sub' => $request->deskripsi_sub,
-            'waktu_sub' => $request->waktu_sub,
-            'harga_sub' => $request->harga_sub
-        ]
-    );
+    {
+        $sublayanan = SubLayanan::where('id', $id)->first();
+        $sublayanan->update(
+            [
+                'layanan_id' => $request->layanan_id,
+                'nama_sub' => $request->nama_sub,
+                'deskripsi_sub' => $request->deskripsi_sub,
+                'waktu_sub' => $request->waktu_sub,
+                'harga_sub' => $request->harga_sub
+            ]
+        );
         
-    $validasi = $request->validate([
-        'ikon_sub' => 'required|mimes:jpg,bmp,png,svg,jpeg|max:2560',
-    ]);
+        $validasi = $request->validate([
+            'ikon_sub' => 'required|mimes:jpg,bmp,png,svg,jpeg|max:2560 ',
+        ]);
 
-    $file = $request->file('ikon_sub');
-    $sublayanan->ikon_sub = time().'_'.$file->getClientOriginalName();
-    $sublayanan->update();
-    $nama_file = time().'_'.$file->getClientOriginalName();
+        $file = $validasi[('ikon_sub')];
+        $sublayanan->ikon_sub = time().'_'.$file->getClientOriginalName();
+        $sublayanan->update();
+        $nama_file = time().'_'.$file->getClientOriginalName();
 
-    $location = public_path('assets/ikon/');
+        $location = '../public/assets/ikon/';
 
-    $file->move($location, $nama_file);
+        $file->move($location,$nama_file);
 
-    if (auth()->user()->roles_id == 1) {
-        return redirect('super/sublayanan/'.$id.'/edit')->with('sukses', 'Berhasil Edit Data!');
-    } elseif (auth()->user()->roles_id == 2) {
-        return redirect('admin/sublayanan/'.$id.'/edit')->with('sukses', 'Berhasil Edit Data!');
-    } else {
-        return redirect()->back()->with('gagal', 'Gagal Edit Data!');
+        if (auth()->user()->roles_id == 1) {
+            return redirect('super/sublayanan/'.$id.'/edit')->with('sukses', 'Berhasil Edit Data!');
+        } elseif (auth()->user()->roles_id == 2) {
+            return redirect('admin/sublayanan/'.$id.'/edit')->with('sukses', 'Berhasil Edit Data!');
+        }
     }
-}
 
-public function destroy(string $id)
-{
-    $data = SubLayanan::where('id', $id)->first();
-    $data->delete();
+    public function destroy(string $id)
+    {
+        $data = SubLayanan::where('id', $id)->first();
+        $data->delete();
 
-    if (auth()->user()->roles_id == 1) {
-        return redirect('super/sublayanan')->with('sukses', 'Berhasil Hapus Data!');
-    } elseif (auth()->user()->roles_id == 2) {
-        return redirect('admin/sublayanan')->with('sukses', 'Berhasil Hapus Data!');
-    } else {
-        return redirect()->back()->with('gagal', 'Gagal Hapus Data!');
+        if (auth()->user()->roles_id == 1) {
+            return redirect('super/sublayanan')->with('sukses', 'Berhasil Hapus Data!');
+        } elseif (auth()->user()->roles_id == 2) {
+            return redirect('admin/sublayanan')->with('sukses', 'Berhasil Hapus Data!');
+        }
     }
-}
 }

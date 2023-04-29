69f2c7698063bd55eacbe76a62a5d99a62d2bd65
adislksn <adi.120140038@student.itera.ac.id>
2023-04-30 04:04:40 +0700

    Chart udh jadi boy

--- resources/views/admin/pembukuan/index.blade.php
+++ resources/views/admin/pembukuan/index.blade.php
@@ -6,44 +6,41 @@
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-    <div style="width: 50%">
+    <div class="vh-100 d-flex justify-content-center align-items-center pb-5" style="width: 90%; height: 80%; margin: auto;">
         <canvas id="pemasukan-pengeluaran"></canvas>
     </div>
-    <script>
-        var ctx = document.getElementById('pemasukan-pengeluaran').getContext('2d');
-        var chart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: [
-                    @foreach ($pemasukan as $p)
-                        '{{ date('F', mktime(0, 0, 0, $p->month, 1)) }}',
-                    @endforeach
-                ],
-                datasets: [
-                    {
+        <script>
+            var data = {!! json_encode($data) !!};
+            var labels = Object.keys(data);
+            var pemasukanData = [];
+            var pengeluaranData = [];
+
+            for (var i = 0; i < labels.length; i++) {
+                var label = labels[i];
+                var pemasukan = data[label]['pemasukan'] || 0;
+                var pengeluaran = data[label]['pengeluaran'] || 0;
+                pemasukanData.push(pemasukan);
+                pengeluaranData.push(pengeluaran);
+            }
+            var ctx = document.getElementById('pemasukan-pengeluaran').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labels,
+                    datasets: [{
                         label: 'Pemasukan',
-                        data: [
-                            @foreach ($pemasukan as $p)
-                                {{ $p->total }},
-                            @endforeach
-                        ],
-                        backgroundColor: 'rgba(0, 255, 0, 0.5)',
-                        borderColor: 'rgba(0, 255, 0, 1)',
+                        data: pemasukanData,
+                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
                         borderWidth: 1
-                    },
-                    {
+                    }, {
                         label: 'Pengeluaran',
-                        data: [
-                            @foreach ($pengeluaran as $p)
-                                {{ $p->total }},
-                            @endforeach
-                        ],
-                        backgroundColor: 'rgba(255, 0, 0, 0.5)',
-                        borderColor: 'rgba(255, 0, 0, 1)',
+                        data: pengeluaranData,
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
                         borderWidth: 1
-                    }
-                ]
-            },
+                    }]
+                },
             options: {
                 scales: {
                     yAxes: [{
@@ -54,6 +51,6 @@
                 }
             }
         });
-    </script>
+        </script>
 @include('admin.menu')
 @endsection

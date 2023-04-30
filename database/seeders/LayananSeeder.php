bf1177b796e96125aba860598c0f76475ae8f5cd
adislksn <adi.120140038@student.itera.ac.id>
2023-04-30 18:19:33 +0700

    seeder layanan+sub

---
+++ database/seeders/LayananSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Layanan;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LayananSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $layanan = [
+            [
+                'id' => 1,
+                'nama_layanan' => 'Premium Deep Clean',
+                'ikon_layanan' => 'pain-shoes.png',
+                'deskripsi_layanan' => 'Bersihkan sepatu anda dengan kami',
+            ],
+            [
+                'id' => 2,
+                'nama_layanan' => 'Leather Care',
+                'ikon_layanan' => 'pain-shoes.png',
+                'deskripsi_layanan' => 'Bersihkan bahan kulit anda dengan kami',
+            ],
+            [
+                'id' => 3,
+                'nama_layanan' => 'Repaint',
+                'ikon_layanan' => 'pain-shoes.png',
+                'deskripsi_layanan' => 'Perbarui warna barang anda dengan kami',
+            ],
+            [
+                'id' => 4,
+                'nama_layanan' => 'Repair Package',
+                'ikon_layanan' => 'pain-shoes.png',
+                'deskripsi_layanan' => 'Perbaiki barang anda dengan kami',
+            ],
+        ];
+        Layanan::query()->insert($layanan);
+    }
+}
---
+++ database/seeders/SubSeeder.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SubLayanan;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SubSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sublayanan = [
+            [
+                'id' => 1,
+                'layanan_id' => 1,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Medium',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 50000,
+            ],
+            [
+                'id' => 2,
+                'layanan_id' => 1,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Hard 1',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 10000,
+            ],
+            [
+                'id' => 3,
+                'layanan_id' => 1,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Hard 2',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 20000,
+            ],
+            [
+                'id' => 4,
+                'layanan_id' => 1,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'White Shoes',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 50000,
+            ],
+            [
+                'id' => 5,
+                'layanan_id' => 1,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Woman Shoes',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 40000,
+            ],
+            [
+                'id' => 6,
+                'layanan_id' => 1,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Sandals',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 35000,
+            ],
+            [
+                'id' => 7,
+                'layanan_id' => 1,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Kids',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 35000,
+            ],
+            [
+                'id' => 8,
+                'layanan_id' => 1,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Bag Cleaning',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 40000,
+            ],
+            [
+                'id' => 9,
+                'layanan_id' => 1,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Hat',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 35000,
+            ],
+            [
+                'id' => 10,
+                'layanan_id' => 1,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Helmet',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 35000,
+            ],
+            [
+                'id' => 11,
+                'layanan_id' => 2,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Shoes',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 50000,
+            ],
+            [
+                'id' => 12,
+                'layanan_id' => 2,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Bag',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 40000,
+            ],
+            [
+                'id' => 13,
+                'layanan_id' => 2,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Pouch',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 35000,
+            ],
+            [
+                'id' => 14,
+                'layanan_id' => 3,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Canvas',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 150000,
+            ],
+            [
+                'id' => 15,
+                'layanan_id' => 3,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Leather',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 200000,
+            ],
+            [
+                'id' => 16,
+                'layanan_id' => 3,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Repaint Midsole',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 90000,
+            ],
+            [
+                'id' => 17,
+                'layanan_id' => 4,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Unyellowing Midsole',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 90000,
+            ],
+            [
+                'id' => 18,
+                'layanan_id' => 4,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Reglue Package',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 80000,
+            ],
+            [
+                'id' => 19,
+                'layanan_id' => 4,
+                'ikon_sub' => 'pain-shoes.png',
+                'nama_sub' => 'Stitching Package',
+                'deskripsi_sub' => 'Bersihkan sepatu anda dengan kami',
+                'waktu_sub' => 1,
+                'harga_sub' => 80000,
+            ],
+        ];
+        SubLayanan::query()->insert($sublayanan);
+    }
+}

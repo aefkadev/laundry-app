7ded7e80fc4195f67fc3e83be4c18f44fea4c9f2
masQ-21dev <mqomarudin0480@gmail.com>
2023-05-07 13:47:09 +0700

    fix front-end client

--- resources/views/client/sublayanan/index.blade.php
+++ resources/views/client/sublayanan/index.blade.php
@@ -11,24 +11,28 @@
     </section>
     <section class="px-4 body-section d-flex flex-column gap-3 pt-3" style="padding-bottom: 100px">
     @foreach ($sublayanans as $sublayanan)
-            <div class="d-flex align-items-center justify-content-between gap-4 bg-white rounded-4 px-4">
-                @if ($sublayanan->ikon_sub == Null)
-                    <img src="{{ asset('assets/ikon') }}/default.png" alt="ikon" style="width: 4.2rem; height: 4.2rem;" class="p-1"/>
-                @else
-                    <img src="{{ asset('assets/ikon') }}/{{ $sublayanan->ikon_sub }}" alt="ikon" style="width: 4.2rem; height: 4.2rem;" class="p-1"/>
-                @endif
-                <span class="fw-bolder">{{ $sublayanan->nama_sub }}</span>
-                <div class="d-flex gap-2">
-                    <a href="/member/order/{{$sublayanan->id}}">
-                        <button class="btn fw-bold rounded-3" style="background-color: #D6C37E;" id="desc-toggle">
-                            Order
-                        </button>
-                    </a>
-                    <a href="https://wa.me/+6281397575460?text=Halo admin SOC Clean Lampung, Saya {{auth()->user()->nama}}">
-                        <button class="btn fw-bold rounded-3" style="background-color: #D6C37E;" id="Chat">
-                            Chat
-                        </button>
-                    </a>
+            <div class="d-flex align-items-center bg-white rounded-4 px-4 py-2">
+                <div class="d-flex">
+                    @if ($sublayanan->ikon_sub == Null)
+                        <img src="{{ asset('assets/ikon') }}/default.png" alt="ikon" style="width: 4.2rem; height: 4.2rem;" class="p-1"/>
+                    @else
+                        <img src="{{ asset('assets/ikon') }}/{{ $sublayanan->ikon_sub }}" alt="ikon" style="width: 4.2rem; height: 4.2rem;" class="p-1"/>
+                    @endif
+                </div>
+                <div class="col d-flex flex-column flex-md-row justify-content-between"> 
+                    <span class="fw-bolder py-1">{{ $sublayanan->nama_sub }}</span>
+                    <div class="d-flex justify-content-end gap-2">
+                        <a href="/member/order/{{$sublayanan->id}}">
+                            <button class="btn fw-bold rounded-3" style="background-color: #D6C37E;" id="desc-toggle">
+                                Order
+                            </button>
+                        </a>
+                        <a href="https://wa.me/+6281397575460?text=Halo admin SOC Clean Lampung, Saya {{auth()->user()->nama}}">
+                            <button class="btn fw-bold rounded-3" style="background-color: #D6C37E;" id="Chat">
+                                Chat
+                            </button>
+                        </a>
+                    </div>
                 </div>
             </div>
             @endforeach

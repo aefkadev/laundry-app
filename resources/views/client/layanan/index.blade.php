aa203be86a3aed9e4979d4bb4736352f44695005
masQ-21dev <mqomarudin0480@gmail.com>
2023-05-11 10:11:33 +0700

    layana

--- resources/views/client/layanan/index.blade.php
+++ resources/views/client/layanan/index.blade.php
@@ -10,8 +10,25 @@
             <span class="fw-bolder px-2" style="color: #E2DFEB;">Service Pelayanan</span>
         </a>
     </section>
+    <section class="px-4 body-section d-flex flex-column gap-3 pt-3" style="padding-bottom: 100px">
     @foreach ($layanans as $layanan)
-    <section class="row d-flex justify-content-center gap-3 px-4 pt-5">
+        <div class="d-flex align-items-center bg-white rounded-4 px-4 py-2 ">
+                <div class="d-flex">
+                   @if ($layanan->ikon_layanan == Null)
+                        <img src="{{ asset('assets/ikon') }}/default.png" alt="ikon" style="width: 4.2rem; height: 4.2rem;" class="p-1"/>
+                    @else
+                        <img src="{{ asset('assets/ikon') }}/{{ $layanan->ikon_layanan }}" alt="ikon" style="width: 4.2rem; height: 4.2rem;" class="p-1"/>
+                    @endif
+                </div>
+                <div class="col d-flex flex-column justify-content-between"> 
+                    <span class="fw-bolder py-1">{{ $layanan->nama_layanan }}</span>
+                    <div class="font-weight-normal text-black">
+                        <span>deskripsi</span>
+                    </div>
+                </div>
+            </div>
+
+    {{-- <section class="row d-flex justify-content-center gap-3 px-4 pt-5">
         <a href="{{ route('member.m-layanan.show',$layanan->id) }}" class="d-flex align-items-center gap-4 bg-white ps-4 text-decoration-none font-weight-bolder text-black rounded-4">
             @if ($layanan->ikon_layanan == Null)
                 <img src="{{ asset('assets/ikon') }}/default.png" alt="ikon" style="width: 4.2rem; height: 4.2rem;" class="p-1"/>
@@ -20,8 +37,9 @@
             @endif
             <span>{{ $layanan->nama_layanan }}</span>
         </a>
-    </section>
+    </section> --}}
     @endforeach
+    </section>
 </div>
 
 @endsection
